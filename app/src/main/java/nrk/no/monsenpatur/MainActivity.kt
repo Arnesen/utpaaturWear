ab435ff5ca79c34cd4e1090216d780f7df070cba
Alexander Arnesen <alexanderarnesen@gmail.com>
2018-06-05 16:53:56 +0200

    initial commit

---
+++ app/src/main/java/nrk/no/monsenpatur/MainActivity.kt
@@ -0,0 +1,100 @@
+package nrk.no.monsenpatur
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.wearable.activity.WearableActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class MainActivity : WearableActivity(), SensorEventListener {
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun currentTimeStr(): String {
+        val c = Calendar.getInstance()
+        val df = SimpleDateFormat("HH:mm:ss")
+        return df.format(c.time)
+    }
+
+
+    override fun onSensorChanged(event: SensorEvent) {
+        when {
+            event.sensor.type == Sensor.TYPE_HEART_RATE -> {
+                val msg = "" + event.values[0].toInt()
+                heartrate.text = msg
+            }
+            event.sensor.type == Sensor.TYPE_STEP_COUNTER -> {
+                val msg = "" + event.values[0].toInt()
+                steps.text = msg
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            50 -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    setupSensors()
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    finish()
+                }
+                return
+            }
+
+        // Add other 'when' lines to check for other
+        // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BODY_SENSORS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.BODY_SENSORS),
+                    50)
+        }else{
+            setupSensors()
+        }
+
+
+        // Enables Always-on
+        setAmbientEnabled()
+    }
+
+    private fun setupSensors() {
+        val mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val mHeartRateSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE)
+        val mStepCountSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+        val mStepDetectSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)
+
+        mSensorManager.registerListener(this, mHeartRateSensor, SensorManager.SENSOR_DELAY_NORMAL)
+        mSensorManager.registerListener(this, mStepCountSensor, SensorManager.SENSOR_DELAY_FASTEST)
+        mSensorManager.registerListener(this, mStepDetectSensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+}
